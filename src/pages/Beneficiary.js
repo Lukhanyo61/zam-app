997bab0ea46d938ab1eaa6e8ec6aefca6bdf3e9a
Lukhanyo61 <105869393+Lukhanyo61@users.noreply.github.com>
2023-08-16 10:30:02 +0000

    new

--- src/pages/Beneficiary.js
+++ src/pages/Beneficiary.js
@@ -1,12 +1,66 @@
 
-import './App.css';
+import React, { useState } from 'react';
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBCollapse
+ 
+} from 'mdb-react-ui-kit';
+import Banner from './new/Banner';
+import Testimonial from './new/Testimonial';
+import Nominate from './new/Nominate';
+import Footer from '../Components/Footer'
 
-function Beneficiary() {
+
+export default function Navbar() {
+  const [showNav, setShowNav] = useState(false);
+
   return (
-    <div>
-   
-    </div>
+    <>
+    <MDBNavbar expand='lg' light bgColor='light'>
+      <MDBContainer fluid>
+        <MDBNavbarBrand href='#'>Navbar</MDBNavbarBrand>
+        <MDBNavbarToggler
+          type='button'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+          onClick={() => setShowNav(!showNav)}
+        >
+          
+        </MDBNavbarToggler>
+        <MDBCollapse navbar show={showNav}>
+          <MDBNavbarNav>
+            <MDBNavbarItem>
+              <MDBNavbarLink active aria-current='page' href='#'>
+                Home
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='/About'>Features</MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='#'>Pricing</MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
+                Disabled
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+    <Banner />
+    <Testimonial />
+    <Nominate />
+    <br></br>
+    <Footer />
+
+    </>
   );
 }
-
-export default Beneficiary;
